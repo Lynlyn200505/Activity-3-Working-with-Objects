2bbf8e29feedf8e1807beedc61924f673bd0be73
Lynlyn200505 <ailynlucina1727@gmail.com>
2024-10-19 13:51:34 +0800

    Create objectActivity.js

---
+++ objectActivity.js
@@ -0,0 +1,13 @@
+let car = {
+    type: "Sport Car",
+    model: "Camry Hybrid",
+    color: "Maroon"
+};
+
+console.log(typeof car);
+
+car.type = "Toyota";
+console.log(car);
+
+car.wheels = 4;
+console.log(car);
